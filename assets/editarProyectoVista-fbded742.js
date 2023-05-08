ef88120c27d6546827d7f3d4593fbb9f01a6e42c
daw2-pereira23 <113383012+daw2-pereira23@users.noreply.github.com>
2023-05-08 17:27:20 +0200

    Update editarProyectoVista-fbded742.js

--- assets/editarProyectoVista-fbded742.js
+++ assets/editarProyectoVista-fbded742.js
@@ -35,7 +35,7 @@ const editarProyecto = {
       proyecto.descripcion = document.querySelector("#descripcion").value;
       proyecto.enlace = document.querySelector("#enlaceEditar").value;
       proyecto.update(proyecto);
-      window.location.href = '/#/proyectos'
+      window.location.href = 'Proyecto_TrabajoAlumnos/#/proyectos'
     });
     document.querySelector("#cancelarCambios").addEventListener("click", () => {
       console.log("Has pulsado sobre cancelar cambios");
